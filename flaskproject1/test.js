eba3e49e75835790ab54c3680d286ccde3de3ada
cyber33 <103075315+Cauavini5@users.noreply.github.com>
2024-09-10 18:26:08 -0300

    Update test.js

--- flaskproject1/test.js
+++ flaskproject1/test.js
@@ -1,11 +1,1 @@
-const lita = ['CARRÒ']
 
-function normalizeString(str) {
-    return str.toLowerCase().replace(/[\W_]+/g, ''); // Remove pontuação e normaliza case
-}
-
-if (lita.includes('CARRO')) {
-    console.log('yes')
-} else {
-    console.log('not')
-}
